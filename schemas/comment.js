a787f007a84e2b24b0df9b8c806f6647f63117c7
dayounglee <verdantjuly@kakao.com>
2023-06-15 12:59:21 +0900

    comments의 스키마에서 user의required:true를 삭제함

--- schemas/comment.js
+++ schemas/comment.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose")
 
 const commentScheama = new mongoose.Schema({
     user: {
-        type: String,
-        unique: true
+        type: String
     },
 
     password: {
